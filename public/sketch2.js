be9bafdec4032382e6674fa3c3a32e11bb734262
Dizotoff <izotov.dima@gmail.com>
2018-04-05 18:02:04 +0300

    responsive window, visuals 123456 controls, size of the window control

--- public/sketch2.js
+++ public/sketch2.js
@@ -1,17 +1,18 @@
 var mic, fft, vhs, cam;
 let angle = 0;
-let c =5;
+let c =4;
 let speed =0;
 let autocontrol = false;
 let rotateCtr = true;
+var r = 1;
 function setup() {
   createCanvas(windowWidth-c, windowHeight-c);
-  smiley = loadImage("spiral.jpg");
+  smiley = loadImage("spiralBlknWht.jpg");
   vhs= createVideo(["vhs.mp4"]);
   vhs.loop();
 vhs.hide();
-  cam = createCapture(VIDEO);
-  cam.hide();
+cam = createCapture(VIDEO);
+cam.hide();
 
    mic = new p5.AudioIn();
    mic.start();
@@ -31,7 +32,7 @@ pop();
 //bottom left
 push();
 var spectrum = fft.analyze();
-translate(-10,0);
+translate(-2,0);
    beginShape();
    for (i = 0; i<spectrum.length; i++) {
     vertex(i, map(spectrum[i], 0, 255, height, 0) );
@@ -42,7 +43,7 @@ pop();
 //bottom right
 push();
 
-translate(500,0);
+translate(windowWidth-1024,0);
    beginShape();
 
    for (i = 0; i<spectrum.length; i++) {
@@ -55,7 +56,7 @@ pop();
 //right top
 push();
 
-translate(500,-850);
+translate(windowWidth-1024,-windowHeight);
 
    beginShape();
 
@@ -69,7 +70,7 @@ pop();
 //left top
 push();
 
-translate(-0,-900);
+translate(-2,-windowHeight);
 
    beginShape();
 
@@ -85,7 +86,7 @@ push();
 translate(windowWidth/2, windowHeight/2);
 rotate(angle);
 imageMode(CENTER);
-image(smiley, 0, 0, 960/2.5*1.75, 540/1.85*1.7);
+image(smiley, 0, 0, 960/2.5*1.75*r, 540/1.85*1.7*r);
 
 
 pop();
@@ -98,9 +99,9 @@ pop();
 if (autocontrol == true) {
 console.log("TRUEE AUTOCONTROL");
 
-  speed = speed + 0.0015;
+  speed = speed + 0.001;
 
-  if (speed > 2) {
+  if (speed > 4) {
   rotateCtr = false
   }
 
@@ -108,7 +109,7 @@ console.log("TRUEE AUTOCONTROL");
     speed = speed - 0.002;
   }
 
-  if (speed < 0) {
+  if (speed < 0.3) {
     rotateCtr=true;
   }
 } else {
@@ -131,6 +132,10 @@ function speedcontrol() {
     speed = speed-0.001;
   } else if (keyCode === 82) {
     location.reload();
+  } else if (keyCode === UP_ARROW) {
+    r = r+0.01;
+  } else if (keyCode === DOWN_ARROW) {
+    r = r-0.01;
   } else if (keyCode === CONTROL) {
 
 
@@ -150,7 +155,28 @@ function speedcontrol() {
     autocontrol = true;
   } else if (keyCode === 81) {
     autocontrol = false;
+  } else if (keyCode === 49) {
+    smiley = loadImage("spiralBlknWht.jpg");
+  } else if (keyCode === 50) {
+    smiley = loadImage("spiral.jpg");
+  } else if (keyCode === 48) {
+    smiley = createCapture(VIDEO);
+    smiley.hide();
+  } else if (keyCode === 51) {
+    smiley = loadImage("spiral2.jpg");
+  } else if (keyCode === 52) {
+    smiley = loadImage("spiral3.jpg");
+  } else if (keyCode === 53) {
+    smiley = loadImage("spiral4.jpg");
+  } else if (keyCode === 54) {
+    smiley = loadImage("spiral5.png");
+  } else if (keyCode === 55) {
+    smiley = loadImage("spiral6.png");
   }
+
+
+
+
 
 
   return false;
